51bf854a00b6eca52abbcd2b19cb2b2b5f2dcd86
KhatriD <khatridhruv95@gmail.com>
2023-07-21 11:39:53 +0530

    Updated the demo file

--- Code/Demo.m
+++ Code/Demo.m
@@ -1,11 +1,13 @@
 %% Written By Dhruv Khatri, IISER Pune, Cycels Lab  
 clear; close all; 
-inputFile = "../Demo/61 - dyn1-20002 (copy).nd2 (series 1).mat"; 
+inputFile = "../DemoFiles/61 - dyn1-20002 (copy).nd2 (series 1).mat"; 
 A = load(inputFile); % Load the data_struct 
 
 data_struct = A.data_struct;
-frameIgnore = []; 
+frameIgnore = [17]; 
 frameEdit = []; 
-resfullPath = './'; 
+frameInvert = []; 
+resfullPath = './DemoOutput'; 
 
-resolveCoordinates = SingleFilamentResolver(data_struct, frameIgnore, frameEdit, resfullPath); 
+resolveCoordinates = SingleFilamentResolver(data_struct, frameIgnore, ...
+    frameEdit, frameInvert , resfullPath); 
