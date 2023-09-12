fb08f1b63f752b31c7a287318d179f73a3bf5754
KhatriD <khatridhruv95@gmail.com>
2023-09-12 16:37:09 +0530

    updated OPtimizerParameters.m to output time series

--- Code/OptimizeParameters.m
+++ Code/OptimizeParameters.m
@@ -27,6 +27,10 @@ logPs = 0.5; % filter parameter sigma
 invertSeries = false; % Invert series flag
 InvertManual = false; % Manual inversion flag
 tuneParam = true; % Flag to indicate parameter tuning
+
+outputPath =fullfile(inputPath,'DemoOutput');
+[status, msg, msgID] = mkdir(outputPath); 
+segfileName = replace(inputFile,".tiff", "segOverlay.tiff"); 
 %% Adjusting parameters and image processing
 if tuneParam
     % Read and preprocess the image
@@ -53,7 +57,78 @@ if tuneParam
     overImage2 = imoverlay(overImage1, skelImage, 'red');
     figure(2), imshow(overImage2);
 end
+%% Save the initial output movie and check for segmentation errors. 
+selectedFilament = false; 
+for f = 1:length(nFrames)
+    I = imread(filepath,f);
+    I = imadjust(I);
+    medI = medfilt2(I); 
+     if strcmp(useLogP, "TRUE") 
+        fil = -fspecial("log", logPk,logPs); 
+        medF = imfilter(medI, fil); 
+        BW1 = imbinarize(medF);
+        %figure(2), imshow(BW1); 
+    else
+        % Threshold and contour optimization 
+        BW1 = imbinarize(medI, threshold);
+        %figure(2), imshow(BW1); 
+     end 
 
+    actCont = activecontour(medI, BW1, numIteration, 'Chan-vese', ...
+        'ContractionBias',contB, 'SmoothFactor',smoothF);
+    connectedComp = bwconncomp(actCont,8);
+    filaprops = regionprops(connectedComp, 'PixelIdxList'); 
+    if ~selectedFilament
+        overImage1 = imoverlay(medI, bwperim(actCont)); 
+        figure(2), imshow(overImage1); title('Select one filamnet to track'); 
+        [x, y] = ginput(1);
+        indxSelect = sub2ind(size(I),ceil(y), ceil(x)); 
+        for m = 1:length(filaprops)
+            if ismember(indxSelect, filaprops(m).PixelIdxList)
+                selectedFilament = filaprops(m).PixelIdxList;
+            end 
+        end
+    end  
+    nullImage = zeros(size(I)); 
+    for l = 1:length(filaprops)
+        if any(ismember(filaprops(l).PixelIdxList , selectedFilament))
+            nullImage(filaprops(l).PixelIdxList)=1;
+            selectedFilament = filaprops(l).PixelIdxList; 
+        end 
+    end 
+
+    skelImage = bwskel(logical(nullImage));
+    bp = find(bwmorph(skelImage, 'branchpoints')); 
+
+    % Set the fixed tip (Feb/08/2023) 
+    E1 = find(bwmorph(skelImage, 'endpoints')); 
+    [ye, xe] = ind2sub(size(skelImage), E1); 
+    
+    dist_matrix = pdist2([ye, xe], [y, x]); 
+    [~, indxm] = min(dist_matrix, [],1);
+    referenceEnd = E1(indxm); 
+    data_struct(f).FixedEnd = referenceEnd; 
+    % Set the fixed tip 
+
+
+    data_struct(f).Contour = find(skelImage); % save only skeleton indices
+    data_struct(f).FrameNumber = f;
+    data_struct(f).ImageSize = size(I); 
+    if isempty(bp) 
+        data_struct(f).FilamentType = 'Unbranched'; 
+    else
+        data_struct(f).FilamentType = 'Branched';
+    end 
+    figure(1), imshow(imoverlay(medI, skelImage, 'red'),'InitialMagnification', 200,'Border','loose'); title(num2str(f));
+    title(num2str(f))
+    
+    imgData = getframe(gcf).cdata; 
+    imgData = imresize(imgData,size(I)); 
+    imwrite(imgData, fullfile(outputPath, segfileName),'Writemode', 'append');
+
+    %patch([xx' nan], [yy' nan], [yy' nan], [yy' nan], 'edgecolor', 'interp'); 
+    %pause(0.25)
+end 
 %% Output Final segmentation parameters
 % Define headers for the table
 headers = {'Filename', 'FolderPath', 'segFile', 'segThresh', 'segIteration', ...
